c9383f8f3a4bb19818d311d402fa2cb1206c1c21
Joel Arvidsson <joel@oblador.se>
2015-09-29 20:54:47 -0400

    Deprecate react-tween-state fallback and add support for RN 0.12.

--- Collapsible.js
+++ Collapsible.js
@@ -5,7 +5,6 @@
 'use strict';
 
 var React = require('react-native');
-var tweenState = require('react-tween-state');
 var {
   Animated,
   Easing,
@@ -14,9 +13,10 @@ var {
 
 var ANIMATED_EASING_PREFIXES = ['easeInOut', 'easeOut', 'easeIn'];
 
-var Collapsible = React.createClass({
-  mixins: [tweenState.Mixin],
+// For some reason 0 heights won't hide overflow in RN 0.12+
+var ALMOST_ZERO = 0.00000001;
 
+var Collapsible = React.createClass({
   propTypes: {
     collapsed:  React.PropTypes.bool,
     duration:   React.PropTypes.number,
@@ -42,78 +42,65 @@ var Collapsible = React.createClass({
 
   getInitialState() : Object {
     return {
-      height: (Animated ? new Animated.Value(0) : 0),
+      height: new Animated.Value(ALMOST_ZERO),
       contentHeight: 0,
     };
   },
 
   _toggleCollapsed(collapsed : bool) {
-    var height = collapsed ? 0 : this.state.contentHeight;
+    var height = collapsed ? ALMOST_ZERO : this.state.contentHeight;
     var { easing, duration } = this.props;
 
     if(typeof easing === 'string') {
-      if(Easing) {
-        var prefix, found = false;
-        for (var i = 0; i < ANIMATED_EASING_PREFIXES.length; i++) {
-          prefix = ANIMATED_EASING_PREFIXES[i];
-          if(easing.substr(0, prefix.length) === prefix) {
-            easing = easing.substr(prefix.length, 1).toLowerCase() + easing.substr(prefix.length + 1);
-            prefix = prefix.substr(4, 1).toLowerCase() + prefix.substr(5);
-            easing = Easing[prefix](Easing[easing || 'ease']);
-            found = true;
-            break;
-          }
-        };
-        if(!found) {
-          easing = Easing[easing];
+      var prefix, found = false;
+      for (var i = 0; i < ANIMATED_EASING_PREFIXES.length; i++) {
+        prefix = ANIMATED_EASING_PREFIXES[i];
+        if(easing.substr(0, prefix.length) === prefix) {
+          easing = easing.substr(prefix.length, 1).toLowerCase() + easing.substr(prefix.length + 1);
+          prefix = prefix.substr(4, 1).toLowerCase() + prefix.substr(5);
+          easing = Easing[prefix](Easing[easing || 'ease']);
+          found = true;
+          break;
         }
-      } else {
-        easing = tweenState.easingTypes[easing];
+      };
+      if(!found) {
+        easing = Easing[easing];
       }
       if(!easing) {
         throw new Error('Invalid easing type "' + this.props.easing +'"');
       }
     }
 
-    if(Animated) {
-      if(this._animation) {
-        this._animation.stop();
-      }
-      this._animation = Animated.timing(this.state.height, {
-        toValue: height,
-        duration,
-        easing,
-      }).start();
-    } else {
-      this.tweenState('height', {
-        easing:   easing,
-        duration: this.props.duration,
-        endValue: height,
-      });
+    if(this._animation) {
+      this._animation.stop();
     }
+    this._animation = Animated.timing(this.state.height, {
+      toValue: height,
+      duration,
+      easing,
+    }).start();
   },
 
   _handleLayoutChange(event : Object) {
     var contentHeight = event.nativeEvent.layout.height;
-    var height = this.props.collapsed ? 0 : contentHeight
+    var height = this.props.collapsed ? ALMOST_ZERO : contentHeight
     this.setState({
-      height: (Animated ? new Animated.Value(height) : height),
+      height: new Animated.Value(height),
       contentHeight,
     });
   },
 
   render() {
-    var Container = (Animated ? Animated.View : View);
     var style = {
       overflow: 'hidden',
-      height: (Animated ? this.state.height : this.getTweeningValue('height'))
+      height: this.state.height
     };
     return (
-      <Container style={style} pointerEvents={this.props.collapsed ? 'none' : 'auto'}>
+      <Animated.View style={style} pointerEvents={this.props.collapsed ? 'none' : 'auto'}>
         <View ref="content" onLayout={this._handleLayoutChange}>
           {this.props.children}
         </View>
-      </Container>
+      </Animated.View>
     );
   }
 });
