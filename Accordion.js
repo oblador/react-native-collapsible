6cfbbc34c67d5eb512bebcce8c1ac599422439d5
olahat90 <linkid.attila@gmail.com>
2019-04-02 13:21:51 +0200

    Accordion in loop

--- Accordion.js
+++ Accordion.js
@@ -100,11 +100,15 @@ export default class Accordion extends Component {
     return (
       <View style={containerStyle} {...viewProps}>
         {sections.map((section, key) => {
-          const concatKey = (loopIndex ? (loopIndex + '_' + key) : key);
+          const concatKey = (loopIndex ? loopIndex + '_' + key : key);
 
           return (
             <View key={concatKey} style={sectionContainerStyle}>
-              {renderSectionTitle(section, concatKey, activeSections.includes(concatKey))}
+              {renderSectionTitle(
+              	section,
+              	concatKey,
+              	activeSections.includes(concatKey)
+              )}
 
               {expandFromBottom && renderCollapsible(section, concatKey)}
 
